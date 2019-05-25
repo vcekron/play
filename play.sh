6baf12bc13d947c4881e35a51f85f1e2078a67bb
danikron <danikron@pm.me>
2019-05-26 01:55:02 +0200

    Removed needless linebreak

--- play.sh
+++ play.sh
@@ -111,13 +111,13 @@ PATTERN="$@"
 
 # Fetch media index
 for moviedir in ${MOVIES[@]}; do
-	MOVIEDIRS+="$(find -L "$moviedir" -maxdepth 1 -type d | grep -v "\$RECYCLE.BIN")$'\n'"
+	MOVIEDIRS+="$(find -L "$moviedir" -maxdepth 1 -type d | grep -v "\$RECYCLE.BIN")"
 done
 
 MOVIEDIRS=$(echo "$MOVIEDIRS" | awk '{FS="/" ; $0=$0 ; print $NF"|"$0}' | sort -t/ -k1 | cut -d"|" -f2 | grep -v '^$')
 
 for seriesdir in ${SERIES[@]}; do
-	SERIESDIRS+="$(find -L "$seriesdir" -maxdepth 1 -type d | grep -v "\$RECYCLE.BIN")$'\n'"
+	SERIESDIRS+="$(find -L "$seriesdir" -maxdepth 1 -type d | grep -v "\$RECYCLE.BIN")"
 done
 
 SERIESDIRS=$(echo "$SERIESDIRS" | awk '{FS="/" ; $0=$0 ; print $NF"|"$0}' | sort -t/ -k1 | cut -d"|" -f2 | grep -v '^$')
